c9589f282274d4a38f71cfdaec0b7f070db3633c
Cross Medders <crossmedders@gmail.com>
2021-03-29 14:02:13 -0400

    animatio nagain

--- HappyLabels.py
+++ HappyLabels.py
@@ -2,13 +2,13 @@ from UserInputWindow import Ui_MainWindow
 from Label import Label
 
 from PySide2 import QtWidgets, QtCore, QtGui
-"""import pyqtgraph.opengl as gl"""
+import pyqtgraph.opengl as gl
 from PySide2.QtGui import QIcon, QImage, QPixmap
 from PySide2.QtWidgets import  QMainWindow, QAction
 from EditStuff import EditStuff
 from datetime import date
 import numpy as np
-"""from opensimplex import OpenSimplex"""
+from opensimplex import OpenSimplex
 import sys
 from data.connection import createConnection
 class HappyLabels(QMainWindow, Ui_MainWindow):
@@ -16,7 +16,44 @@ class HappyLabels(QMainWindow, Ui_MainWindow):
         super(HappyLabels, self).__init__()
         self.setupUi(self)
         sys.stdout = self
-    
+         
+        self.w = gl.GLViewWidget()
+        self.w.setGeometry(0, 110, 1920, 1080 )
+        self.w.show()
+        self.nsteps = 1
+        self.ypoints = range(-20, 22, self.nsteps)
+        self.xpoints = range(-20, 22, self.nsteps)
+        self.nfaces = len(self.ypoints)
+        self.offset = 0
+        self.tmp = OpenSimplex()
+        verts = np.array([
+            [
+                x, y, 1.5 * self.tmp.noise2d(x=n / 5, y=m / 5)
+            ] for n, x in enumerate(self.xpoints) for m, y in enumerate(self.ypoints)
+        ], dtype=np.float32)
+        faces = []
+        colors = []
+        for m in range(self.nfaces - 1):
+            yoff = m * self.nfaces
+            for n in range(self.nfaces - 1):
+                faces.append([n + yoff, yoff + n + self.nfaces, yoff + n + self.nfaces + 1])
+                faces.append([n + yoff, yoff + n + 1, yoff + n + self.nfaces + 1])
+                colors.append([0, 0, 0, 0])
+                colors.append([0, 0, 0, 0])
+
+        faces = np.array(faces)
+        colors = np.array(colors)
+
+        # create the mesh item
+        self.m1 = gl.GLMeshItem(
+            vertexes=verts,
+            faces=faces, faceColors=colors,
+            smooth=False, drawEdges=True,
+        )
+        self.m1.setGLOptions('additive')
+        self.w.addItem(self.m1)
+
+        self.pushButton.clicked.connect(self.OpenLabels)
         self.pushButton.clicked.connect(self.OpenLabels)
         self.comboBox.currentTextChanged.connect(self.SetLabels)
         self.setWindowTitle("Sup")
@@ -35,16 +72,16 @@ class HappyLabels(QMainWindow, Ui_MainWindow):
         self.label = None
         self.editthing = None
         self.createMenu()
-    """def start(self):
+    def start(self):
         
-        get the graphics window open and setup
+       
         
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
             QtGui.QApplication.instance().exec_()
 
     def update(self):
         
-        update the mesh and shift the noise each time
+        
         
         verts = np.array([
             [
@@ -75,7 +112,7 @@ class HappyLabels(QMainWindow, Ui_MainWindow):
         timer.timeout.connect(self.update)
         timer.start(10)
         self.start()
-        self.update()"""
+        self.update()
     def createMenu(self):
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('File')
@@ -147,5 +184,5 @@ if __name__ == '__main__':
     main = HappyLabels()
 
     main.show()
-    """main.animation()"""
+    main.animation()
     sys.exit(app.exec_())
